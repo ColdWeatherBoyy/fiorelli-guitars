6a8d37a22e751b319653d4f597f530dd3999006e
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2024-06-13 21:42:56 -0400

    responsive styling updates

--- app/components/components/Card.tsx
+++ app/components/components/Card.tsx
@@ -9,17 +9,19 @@ interface CardProps {
 
 const Card: FC<CardProps> = ({ title, body, children }) => {
 	return (
-		<div className="overflow-scroll rounded-sm bg-gradient-to-br from-cyan-50/80 to-zinc-300/80 shadow shadow-cyan-100/80 backdrop-blur-md px-8 py-6 transform transition">
+		<div className="overflow-scroll rounded-sm bg-gradient-to-br from-cyan-50/80 to-zinc-300/80 shadow shadow-cyan-100/80 backdrop-blur-md px-6 py-4 md:px-8 md:py-6 transform transition">
 			{title && (
 				<div
-					className={`${roboto_mono.className} border-b pb-2 mb-4 border-cyan-800 text-4xl font-semibold`}
+					className={`${roboto_mono.className} text-center border-b pb-2 mb-4 border-cyan-800 text-2xl md:text-4xl font-semibold`}
 				>
 					{title}
 				</div>
 			)}
 			{body.map((paragraph, index) => (
 				<div key={index}>
-					<div className={`text-lg ${children && "last:mb-4"}`}>{paragraph}</div>
+					<div className={`text-base md:text-lg ${children && "last:mb-4"}`}>
+						{paragraph}
+					</div>
 					{index < body.length - 1 && (
 						<div className="my-4 h-[1px] w-full bg-cyan-600/40" />
 					)}
