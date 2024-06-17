cd11c68835fcb315fe7f47ba88c2368672fae9f8
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2024-06-17 18:06:19 -0400

    successfully implemented useFormState

--- app/contact/ContactForm.tsx
+++ app/contact/ContactForm.tsx
@@ -1,35 +1,50 @@
+"use client";
+
 import { TextSize } from "@/app/utilities/types";
 import CardButtonLink from "../components/components/CardButtonLink";
+// import { createUserAndMessage } from "../utilities/databaseActions";
+import { PrismaClient } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { useFormState } from "react-dom";
 import { createUserAndMessage } from "../utilities/databaseActions";
 
 const ContactForm = () => {
+	const [data, formAction] = useFormState(createUserAndMessage, false);
 	return (
-		<form action={createUserAndMessage} className="flex flex-col w-full">
-			<label className="mb-2 font-semibold">Name:</label>
-			<input
-				type="text"
-				required
-				placeholder="Name"
-				name="name"
-				className={`mb-4 p-2 rounded-sm bg-zinc-100 dark:bg-zinc-700 shadow-inner shadow-zinc-400 dark:shadow-zinc-800 focus:shadow focus:shadow-zinc-500 dark:focus:shadow-zinc-900 focus:outline-0 focus:ring-1 focus:ring-cyan-500/50 dark:focus:ring-cyan-600/60 focus:ring-opacity-50`}
-			/>
-			<label className="mb-2 font-semibold">Email:</label>
-			<input
-				type="email"
-				required
-				placeholder="Email"
-				name="email"
-				className={`mb-4 p-2 rounded-sm bg-zinc-100 dark:bg-zinc-700 shadow-inner shadow-zinc-400 dark:shadow-zinc-800 focus:shadow focus:shadow-zinc-500 dark:focus:shadow-zinc-900 focus:outline-0 focus:ring-1 focus:ring-cyan-500/50 dark:focus:ring-cyan-600/60 focus:ring-opacity-50`}
-			/>
-			<label className="mb-2 font-semibold">What are you looking for?</label>
-			<textarea
-				placeholder="Message"
-				required
-				name="message"
-				className={`mb-4 p-2 rounded-sm bg-zinc-100 dark:bg-zinc-700 shadow-inner shadow-zinc-400 dark:shadow-zinc-800 focus:shadow focus:shadow-zinc-400 dark:focus:shadow-zinc-900 focus:outline-0 focus:ring-1 focus:ring-cyan-500/50 dark:focus:ring-cyan-600/60 focus:ring-opacity-50 resize-none h-24`}
-			></textarea>
-			<CardButtonLink text="Submit" size={TextSize.small} />
-		</form>
+		<>
+			{!data ? (
+				<form action={formAction} className="flex flex-col w-full">
+					<label className="mb-2 font-semibold">Name:</label>
+					<input
+						type="text"
+						required
+						placeholder="Name"
+						name="name"
+						className={`mb-4 p-2 rounded-sm bg-zinc-100 dark:bg-zinc-700 shadow-inner shadow-zinc-400 dark:shadow-zinc-800 focus:shadow focus:shadow-zinc-500 dark:focus:shadow-zinc-900 focus:outline-0 focus:ring-1 focus:ring-cyan-500/50 dark:focus:ring-cyan-600/60 focus:ring-opacity-50`}
+					/>
+					<label className="mb-2 font-semibold">Email:</label>
+					<input
+						type="email"
+						required
+						placeholder="Email"
+						name="email"
+						className={`mb-4 p-2 rounded-sm bg-zinc-100 dark:bg-zinc-700 shadow-inner shadow-zinc-400 dark:shadow-zinc-800 focus:shadow focus:shadow-zinc-500 dark:focus:shadow-zinc-900 focus:outline-0 focus:ring-1 focus:ring-cyan-500/50 dark:focus:ring-cyan-600/60 focus:ring-opacity-50`}
+					/>
+					<label className="mb-2 font-semibold">What are you looking for?</label>
+					<textarea
+						placeholder="Message"
+						required
+						name="message"
+						className={`mb-4 p-2 rounded-sm bg-zinc-100 dark:bg-zinc-700 shadow-inner shadow-zinc-400 dark:shadow-zinc-800 focus:shadow focus:shadow-zinc-400 dark:focus:shadow-zinc-900 focus:outline-0 focus:ring-1 focus:ring-cyan-500/50 dark:focus:ring-cyan-600/60 focus:ring-opacity-50 resize-none h-24`}
+					></textarea>
+					<CardButtonLink text="Submit" size={TextSize.small} />
+				</form>
+			) : (
+				<div className="flex flex-col items-center text-center">
+					<p>Thank you for your message!</p> <p>We'll get back to you ASAP.</p>{" "}
+				</div>
+			)}
+		</>
 	);
 };
 
--- app/utilities/databaseActions.ts
+++ app/utilities/databaseActions.ts
@@ -4,31 +4,39 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const createUserAndMessage = async (formData: FormData) => {
-	let userId;
-	const existingUser = await prisma.user.findUnique({
-		where: {
-			email: formData.get("email") as string,
-		},
-	});
-	if (!existingUser) {
-		const newUser = await prisma.user.create({
-			data: {
-				name: formData.get("name") as string,
+export const createUserAndMessage = async (prevState: boolean, formData: FormData) => {
+	try {
+		let userId;
+		const existingUser = await prisma.user.findUnique({
+			where: {
 				email: formData.get("email") as string,
 			},
 		});
-		userId = newUser.id;
-	} else {
-		userId = existingUser.id;
+
+		if (!existingUser) {
+			const newUser = await prisma.user.create({
+				data: {
+					name: formData.get("name") as string,
+					email: formData.get("email") as string,
+				},
+			});
+			userId = newUser.id;
+		} else {
+			userId = existingUser.id;
+		}
+		const newMessage = await prisma.message.create({
+			data: {
+				content: formData.get("message") as string,
+				userId: userId,
+			},
+		});
+		console.log(newMessage);
+		return true;
+	} catch (error) {
+		// To-Do: Handle error more precisely
+		console.error(error);
+		return false;
 	}
-	const newMessage = await prisma.message.create({
-		data: {
-			content: formData.get("message") as string,
-			userId: userId,
-		},
-	});
-	console.log(newMessage);
 };
 
 export const getUsers = async () => {
