bad39721d79a4b33aeb2e8d6d3f780c223d5e3d6
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2024-06-24 17:13:09 -0400

    image added to contacts

--- app/components/components/PhotoCard.tsx
+++ app/components/components/PhotoCard.tsx
@@ -6,20 +6,22 @@ import { CloudinaryResource, TextSize } from "@/app/utilities/types";
 import { CldImage } from "next-cloudinary";
 import { FC } from "react";
 
-interface PhotoCardProps {
-	photoResource: CloudinaryResource;
-}
-
-const PhotoCard: FC<PhotoCardProps> = ({ photoResource }) => {
+const PhotoCard: FC<CloudinaryResource> = ({
+	width,
+	height,
+	secure_url,
+	public_id,
+	blurDataUrl,
+}) => {
 	return (
 		<Card>
 			<div className="max-h-full flex flex-col gap-6 justify-center items-center">
 				<CldImage
-					width={photoResource.width}
-					height={photoResource.height}
-					src={photoResource.secure_url}
-					alt={photoResource.public_id}
-					blurDataURL={photoResource.blurDataUrl}
+					width={width}
+					height={height}
+					src={secure_url}
+					alt={public_id}
+					blurDataURL={blurDataUrl}
 					placeholder="blur"
 					className="object-scale-down max-h-[67vh] md:max-h-[63vh] w-fit rounded-sm bg-zinc-200 shadow shadow-zinc-500"
 				/>
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,29 @@
 import AnimateWrapper from "../components/components/AnimateWrapper";
 import Card from "../components/components/Card";
 import ContactForm from "./components/ContactForm";
+import { cloudinary } from "../utilities/cloudinary";
+import { getBlurDataUrl } from "../utilities/imageHelpers";
+import WelcomeImage from "../welcome/components/WelcomeImage";
+import GalleryImage from "../gallery/components/GalleryImage";
 
-export default function Contact() {
+export default async function Contact() {
+	const { resources } = await cloudinary.search
+		.expression(`tags=contact_form`)
+		.with_field("context")
+		.execute();
+
+	const blurDataUrl = await getBlurDataUrl(resources[0].public_id);
+	const welcomeImage = {
+		...resources[0],
+		blurDataUrl,
+	};
 	return (
 		<AnimateWrapper>
 			<Card title={"Contact Fiorelli"}>
-				<ContactForm />
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-6 items-center">
+					<GalleryImage {...welcomeImage} />
+					<ContactForm />
+				</div>
 			</Card>
 		</AnimateWrapper>
 	);
