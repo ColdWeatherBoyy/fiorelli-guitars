fd5a422b027953d292e486cd2248e07dab69cb6d
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2024-07-04 00:57:41 -0400

    receives session data from auth

--- app/(admin)/admin/dashboard/page.tsx
+++ app/(admin)/admin/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import { auth } from "@/auth";
 import Title from "../../components/Title";
 
 export default async function AdminHome() {
-	return <Title title="Admin Dashboard" />;
+	const session = await auth();
+	return (
+		<>
+			<Title title="Admin Dashboard" />
+			<div className="text-lg">Welcome, {session?.user?.name?.split(" ")[0]}</div>
+		</>
+	);
 }
