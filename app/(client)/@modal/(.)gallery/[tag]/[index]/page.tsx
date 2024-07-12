118c956beb4abc05551a9cb82fb911c16935d744
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2024-07-12 14:51:39 -0400

    trying to make generateStaticParams functional

--- app/(client)/@modal/(.)gallery/[tag]/[index]/page.tsx
+++ app/(client)/@modal/(.)gallery/[tag]/[index]/page.tsx
@@ -6,14 +6,24 @@ import { CloudinaryResource, GalleryPhotoProps, TextSize } from "@/app/utilities
 import ModalWrapper from "./ModalWrapper";
 
 export async function generateStaticParams() {
-	const { resources } = await cloudinary.search
-		.expression(`tags=gallery`)
+	const { resources: resourcesEJ } = await cloudinary.search
+		.expression(`tags=EJ_Guitar`)
+		.with_field("context")
+		.execute();
+	const { resources: resourcesSP } = await cloudinary.search
+		.expression(`tags=SP_Guitar`)
 		.with_field("context")
 		.execute();
 
-	return resources.map((resource: CloudinaryResource) => ({
-		id: resource.public_id,
+	const params = resourcesEJ.map((resource: CloudinaryResource, index: number) => ({
+		tag: "EJ_Guitar",
+		index: index.toString(),
 	}));
+	const params2 = resourcesSP.map((resource: CloudinaryResource, index: number) => ({
+		tag: "SP_Guitar",
+		index: index.toString(),
+	}));
+	return params.concat(params2);
 }
 
 const PhotoModal: React.FC<GalleryPhotoProps> = async ({ params: { tag, index } }) => {
--- app/(client)/gallery/[tag]/[index]/page.tsx
+++ app/(client)/gallery/[tag]/[index]/page.tsx
@@ -6,13 +6,24 @@ import CardButtonLink from "../../../components/components/CardButtonLink";
 import PhotoCard from "../../../components/components/PhotoCard";
 
 export async function generateStaticParams() {
-	const { resources } = await cloudinary.search
-		.expression(`tags=gallery`)
+	const { resources: resourcesEJ } = await cloudinary.search
+		.expression(`tags=EJ_Guitar`)
 		.with_field("context")
 		.execute();
-	return resources.map((resource: CloudinaryResource) => ({
-		id: resource.public_id,
+	const { resources: resourcesSP } = await cloudinary.search
+		.expression(`tags=SP_Guitar`)
+		.with_field("context")
+		.execute();
+
+	const params = resourcesEJ.map((resource: CloudinaryResource, index: number) => ({
+		tag: "EJ_Guitar",
+		index: index.toString(),
 	}));
+	const params2 = resourcesSP.map((resource: CloudinaryResource, index: number) => ({
+		tag: "SP_Guitar",
+		index: index.toString(),
+	}));
+	return params.concat(params2);
 }
 
 const Photo: React.FC<GalleryPhotoProps> = async ({ params: { tag, index } }) => {
