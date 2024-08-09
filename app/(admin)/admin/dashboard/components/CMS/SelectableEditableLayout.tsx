6bab117841170f326df04c1e9bda16888c77ea98
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2024-08-09 15:50:44 -0400

    adds some features to Guitar CMS, particularly to handle more instruments. Not clear on why the hover to reveal full title is correctly detecting when its truncating, tbh

--- app/(admin)/admin/dashboard/components/CMS/SelectableEditableLayout.tsx
+++ app/(admin)/admin/dashboard/components/CMS/SelectableEditableLayout.tsx
@@ -26,24 +26,33 @@ const SelectEditableLayout: FC<SelectEditableLayoutProps> = ({
 
 	return (
 		<AnimateWrapper>
-			<div className="w-4/5 rounded-md shadow-sm shadow-slate-500 dark:shadow-slate-400">
-				<div className="flex justify-start rounded-t-md w-full text-center">
+			<div className="w-[60dvw] rounded-md shadow-sm shadow-slate-500 dark:shadow-slate-400">
+				<div className="flex flex-reverse overflow-auto justify-start rounded-t-md min-w-4/5 text-center">
 					{titles.map((title, index) => (
 						<div
 							key={index}
 							onClick={() => setSelectedTab(index)}
-							className={`border border-collapse text-lg rounded-t-md border-slate-500 dark:border-slate-400 py-2 cursor-pointer w-full ${
+							className={`relative min-w-[33%] border border-collapse text-lg rounded-t-md border-slate-500 dark:border-slate-400 p-4 cursor-pointer ${
 								selectedTab === index
 									? "bg-slate-100 dark:bg-slate-500 text-xl underline border-b-0 tracking-wider"
 									: "bg-slate-300 dark:bg-slate-800 text-zinc-600 dark:text-zinc-300"
 							}
               `}
 						>
-							{title}
+							<div className="truncate">{title}</div>
+							<div
+								className={`absolute inset-0 rounded-t-md flex items-center justify-center opacity-0 hover:opacity-100 btransition-opacity duration-300 ${
+									selectedTab === index
+										? "bg-slate-100 dark:bg-slate-500 text-xl underline border-b-0 tracking-wider"
+										: "bg-slate-300 dark:bg-slate-800 text-zinc-600 dark:text-zinc-300"
+								}`}
+							>
+								{title}
+							</div>
 						</div>
 					))}
 				</div>
-				<div className="bg-slate-100 dark:bg-slate-500 grid grid-cols-1 sm:grid-cols-2 border-x border-b rounded-b-md border-slate-500 dark:border-slate-400 p-2">
+				<div className="w-full bg-slate-100 dark:bg-slate-500 grid grid-cols-1 sm:grid-cols-2 border-x border-b rounded-b-md border-slate-500 dark:border-slate-400 p-2">
 					{isPageContentArray(content) ? (
 						<EditablePageContentLayout
 							pageContentData={content}
